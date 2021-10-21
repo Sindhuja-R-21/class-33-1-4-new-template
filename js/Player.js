16e6963435163c3506eab24479ecd9e1d6f10b53
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2021-10-21 17:13:16 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  
+  //Bp
+  getCount() {
+   
+  }
+
+  //Bp
+  updateCount(count) {
+   
+  }}
+
